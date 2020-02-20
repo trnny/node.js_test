a27e4297fbb8a59ba6d55b77b462fecf2ada869e
trnny <54834100+trnny@users.noreply.github.com>
2020-02-20 14:07:18 +0800

    Create config.php

---
+++ http_nuc/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// 数据库配置
+$config['db']['host'] = 'host';
+$config['db']['username'] = 'username';
+$config['db']['password'] = 'password';
+$config['db']['dbname'] = 'dbname';
+
+// 默认控制器和操作名
+$config['defaultController'] = 'Controller';
+$config['defaultAction'] = 'index';
+
+return $config;
